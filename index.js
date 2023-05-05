5cb9126a5704d3dfcd76ddd0a7d5811c6c16d439
Piyush24102000 <64774043+Piyush24102000@users.noreply.github.com>
2023-05-05 11:02:56 +0000

    Basics Done

--- index.js
+++ index.js
@@ -91,10 +91,34 @@ class LinkedList{
     }
     this.size++
   }
+  deleteLastNode(){
+    let current = this.head;
+    for(let i = 0 ; i < this.size-2 ; i++){
+      current = current.next
+    }
+      current.next = null
+      this.tail = current
+      this.size--
+    // this.head.next = null
+  }
+  removeAt(ind){
+    if(ind == 0){
+      this.removeFirst()
+    }
+    else{
+      let current = this.head
+      for(let i = 0 ; i < ind-1 ; i++){
+        current = current.next
+      }
+      current.next = current.next.next
+    }
+    this.size--
+  }
 }
 const linkedList = new LinkedList()
 linkedList.addNode(9)
 linkedList.addNode(2)
 linkedList.addNode(3)
-linkedList.insertAtIndex(99,3)
+linkedList.addNode(5)
+linkedList.removeAt(0)
 linkedList.display()
